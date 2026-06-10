595fe01f5dca95b265c4667b6552241fbd81e0fd
Duong Cao <caohaiduong04@gmail.com>
2026-06-10 23:11:08 +0700

    update Our Journey

--- src/shared/database/ourJourney.ts
+++ src/shared/database/ourJourney.ts
@@ -2,51 +2,19 @@ export const OUR_JOURNEY = [
     {
         imageUrl:
             'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '11/2022',
-        year: '2022',
-        title: 'Establishment Startup team',
+        time: '2026',
+        year: '2026',
+        title: 'Start the Business',
         achievements: ['Set up in Ho Chi Minh, Vietnam'],
-    },
-    {
-        imageUrl:
-            'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '06/2023',
-        year: '2023',
-        title: 'Lv.1 Seller on Fiverr',
-        achievements: ['More than 50 customers'],
-    },
-    {
-        imageUrl:
-            'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '09/2023',
-        year: '2023',
-        title: 'Lv.2 Seller on Fiverr',
-        achievements: ['More than 100 customers'],
     },
 ]
 export const VI_OUR_JOURNEY = [
     {
         imageUrl:
             'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '11/2022',
-        year: '2022',
+        time: '2026',
+        year: '2026',
         title: 'Thành lập đội ngũ khởi nghiệp',
         achievements: ['Thành lập tại TP. Hồ Chí Minh, Việt Nam'],
-    },
-    {
-        imageUrl:
-            'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '06/2023',
-        year: '2023',
-        title: 'Người bán Cấp 1 trên Fiverr',
-        achievements: ['Hơn 50 khách hàng'],
-    },
-    {
-        imageUrl:
-            'https://www.superoffice.co.uk/globalassets/home-com-website/resources/articles/visuals/customer-journey/customerjourney_top.jpg',
-        time: '09/2023',
-        year: '2023',
-        title: 'Người bán Cấp 2 trên Fiverr',
-        achievements: ['Hơn 100 khách hàng'],
     },
 ]
